1907d6775d878cfc8a3a62acdcef3b7eb9fab6f2
FerJRL <frl711m@uma.es>
2022-03-17 11:44:52 +0100

    DoubleLinkedListQueue primera version

---
+++ DoubleLinkedListQueue.java
@@ -0,0 +1,53 @@
+public class DoubleLinkedListQueue<T> implements DoubleEndedQueue<T> {
+
+    public DequeNode<T> first, last = null;
+
+    public void append(DequeNode<T> node) {
+
+        last.setNext(node);
+        node.setPrevious(last);
+        node.setNext(null);
+        last = node;
+    }
+
+    public void appendLeft(DequeNode<T> node) {
+
+
+        first.setPrevious(node);
+        node.setPrevious(null);
+        node.setNext(first);
+        first = node;
+    }
+
+
+    public void deleteFirst() {
+        first = first.getNext();
+        first.setPrevious(null);
+    }
+
+
+    public void deleteLast() {
+        last = last.getPrevious();
+        last.setNext(null);
+    }
+
+    public DequeNode<T> peekFirst() {
+        return first;
+    }
+
+    public DequeNode<T> peekLast() {
+        return last;
+    }
+
+    public int size() {
+        int contador = 0;
+        DequeNode<T> actual = peekFirst();
+
+        while(actual != null) {
+            contador++;
+            actual= actual.getNext();
+        }
+
+        return contador;
+    }
+}
